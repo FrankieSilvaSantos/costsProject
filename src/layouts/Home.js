1b4cc1dda83ad1e1ab4b43e0dd7d4ddca95fc3d0
FrankieSilvaSantos <frankiesilvasantos@gmail.com>
2023-06-14 02:11:01 -0300

    home

--- src/layouts/Home.js
+++ src/layouts/Home.js
@@ -1,10 +1,26 @@
+import './Home.css';
+import { Link } from 'react-router-dom';
+
+
 function Home() {
 
     return(    
     
     
     <>
-        <p>HOME</p>
+        <h2 className='titulo-style'>Bem-vindo ao <span className="span-titulo-style">Costs</span></h2>
+
+        <p className='paragrafo-home'>Como gerenciar seus projetos agora mesmo!!</p>
+
+        <section className='container-button-home'>
+            
+        <Link  to={'./novoprojeto'}><button type="button" class="btn button-home-style">Criar Projeto</button></Link>
+        </section>
+
+
+        <section className='container-img-home'>
+        <img src='./img/savings.svg' className='img-home'></img>
+        </section>
     </>
     
     )
